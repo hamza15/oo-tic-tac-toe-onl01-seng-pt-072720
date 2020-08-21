9da64082dfffbbe9dfd8f05992e4e2b017d4bf53
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-08-21 06:00:38 +0000

    Done.

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,92 @@
+class TicTacToe
+  
+  WIN_COMBINATIONS = [[0,1,2],[0,3,6],[0,4,8],[1,4,7],[2,5,8],[2,4,6],[3,4,5],[6,7,8]]
+  
+  def initialize
+    @board = Array.new(9, " ")
+  end 
+  
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+  
+  def input_to_index(index)
+    index.to_i - 1
+  end
+    
+  def move(index, token ="X")
+    @board[index] = token
+  end
+  
+  def position_taken?(index)
+    @board[index] != " "
+  end 
+  
+  def valid_move?(index)
+    index.between?(0,8) && !position_taken?(index)
+  end
+  
+  def turn
+    puts "Pick a position between 1-9"
+    user_input = gets
+    index = input_to_index(user_input)
+
+    if valid_move?(index)
+      token = current_player
+      move(index, token)
+    else
+      turn
+    end
+    display_board
+  end
+  
+  def turn_count
+    @board.count{|i| i != " "}
+  end 
+  
+  def current_player
+    if turn_count.even?
+      "X"
+    else
+      "O"
+    end
+  end
+  
+  def won?
+    WIN_COMBINATIONS.any? do |i|
+      if position_taken?(i[0]) && @board[i[0]] == @board[i[1]] && @board[i[1]] == @board[i[2]]
+      return i
+      end
+    end
+  end
+  
+  def full?
+    @board.all?{|index| index != " "}
+  end
+  
+  def draw?
+    full? && !won?
+  end
+  
+  def over?
+    draw? || won?
+  end 
+  
+  def winner
+    if current_player == "X" && won?
+      return "O"
+    elsif current_player == "O" && won?
+      return "X"
+    end
+  end 
+  
+  def play 
+    turn until over?
+    puts winner ? "Congratulations #{winner}!" : "Cat's Game!"
+  end
+  
+end 
