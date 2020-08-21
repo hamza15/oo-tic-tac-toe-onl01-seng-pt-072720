4492c30dd7974aea7ea0273dcf8a966584c7a5d7
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-08-21 03:32:13 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -25,10 +25,13 @@ class TicTacToe
   def position_taken?(index)
     @board[index] != " "
   end 
-    
   
   def valid_move?(index)
     index.between?(0,8) && !position_taken?(index)
+  end
+  
+  def turn
+    
   end
   
 end 
